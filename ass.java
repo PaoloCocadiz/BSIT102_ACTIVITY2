fd474bb5e1820e705dadc3129a23ac44cbff977f
PaoloCocadiz <pcocadiz@gmail.com>
2019-08-13 09:46:36 +0800

    first commit

---
+++ ass.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ass {
+    
+    public static void main(String[] args) {
+    	
+    	Scanner user_input = new Scanner(System.in);
+    	
+    	String name;
+    	System.out.print("Enter your name: ");
+    	name = user_input.next();    
+  {   	
+    	String Section;
+    	System.out.print("Enter your section: ");
+    	Section = user_input.next();
+  }
+  {
+    	String Address;
+    	System.out.print("Enter your address: ");
+    	Address = user_input.next();
+  }
+  {
+   		String Contact;
+   		System.out.print("Enter your contact number: ");
+   		Contact = user_input.next();
+  }
+  
+}
+}
